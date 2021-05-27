2e962dfb457d9136f675ff73853c798f5eb92b2d
suvofalcon <>
2021-05-27 14:39:30 +0530

    Hello World

--- javamasterclass/src/main/java/com/suvos/javamasterclass/firststeps/HelloWorld.java
+++ javamasterclass/src/main/java/com/suvos/javamasterclass/firststeps/HelloWorld.java
@@ -14,5 +14,14 @@ public class HelloWorld {
     // main method (legendary statement!!)
     public static void main(String[] args){
         System.out.println("Hello World!!");
+        
+        int myFirstNumber = (10 + 5) + (2 * 10); // Declaration Statement
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
+        
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
     }
 }
