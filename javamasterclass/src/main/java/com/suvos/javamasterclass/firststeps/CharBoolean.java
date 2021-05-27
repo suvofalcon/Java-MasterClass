6d6a00f91dfefa78555fb47f0ee02bf4f3d28878
suvofalcon <suvosmac@localhost>
2021-05-27 20:03:49 +0530

    Java Strings

--- javamasterclass/src/main/java/com/suvos/javamasterclass/firststeps/CharBoolean.java
+++ javamasterclass/src/main/java/com/suvos/javamasterclass/firststeps/CharBoolean.java
@@ -25,5 +25,23 @@ public class CharBoolean {
         
         boolean isCustomerOverTwentyOne = true;
         
+        String myString = "This is a String";
+        System.out.println("my string is = "+ myString);
+        myString = myString + ", and another String!!";
+        System.out.println("my string is = "+ myString);
+        myString = myString + "\u00A9 2021";
+        System.out.println("my string is = "+ myString);
+        
+        String numberString = "250.55";
+        numberString = numberString + "49.95";
+        System.out.println(numberString);
+        
+        String lastString = "10";
+        int myInt = 50;
+        lastString = lastString + myInt; // Integer will be converted to String
+        System.out.println("Last String is equal to " + lastString);
+        double doubleNumber = 120.47d;
+        lastString = lastString + doubleNumber;
+        System.out.println("Last String is now "+ lastString);
     }
 }
